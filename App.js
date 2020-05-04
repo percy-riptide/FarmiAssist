4fec79526eb7c5c51492e8296ff514a7fbb15d9c
Percy-Riptide <pratik.p.patil1999@gmail.com>
2020-05-04 10:48:06 +0530

    Splash Screen Edit!

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textcol}>Let's get this started!</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1c1c',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textcol:{
+    color: '#ffffff'
+  }
+});
