2e583d619d84b663dd05ea472e1e5a4d3b501fbb
Percy-Riptide <pratik.p.patil1999@gmail.com>
2020-05-05 17:41:16 +0530

    Get OWM API & Find Latitude, Longitude using User Location

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
+
+  let text1 = 'Waiting..';
+  let text2 = '';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text1 = JSON.stringify(location.coords.longitude);
+    text2 = JSON.stringify(location.coords.latitude);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textcol}>Let's get this started!</Text>
+      <Text style={styles.textcol}>The longiude is:{text1}</Text>
+      <Text style={styles.textcol}>The latitude is:{text2}</Text>
     </View>
   );
 }
---
+++ components/apikey.js
@@ -0,0 +1,1 @@
+export const api_key = "64b268c4b494e9943ef36b68e73f5ed0";
